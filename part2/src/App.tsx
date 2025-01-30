8a80c86c6670c3b86721c106eda0ba383046dd10
makhmudovs <makhmudovs50@gmail.com>
2025-01-30 15:49:41 +0800

    part2:notes

--- part2/src/App.tsx
+++ part2/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./conponents/note/Note";
 
 interface AppItems {
@@ -11,15 +11,46 @@ interface AppProps {
   notes: AppItems[];
 }
 
-const App: React.FC<AppProps> = ({ notes }) => {
+const App: React.FC<AppProps> = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const addNote = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() * 0.5 ? true : false,
+      id: Number(notes.length + 1),
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
   return (
     <>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </>
   );
 };
