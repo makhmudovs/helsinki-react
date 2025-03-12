0582e88814f46e7c1242095e3143a2f69ad11209
makhmudovs <makhmudovs50@gmail.com>
2025-03-13 00:19:08 +0800

    zod validation

--- ilari/src/routes/diaries.ts
+++ ilari/src/routes/diaries.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import diaryService from "../services/diaryService";
-import { NonSensitiveDiaryEntry } from "../types";
-import { toNewDiaryEntry } from "../utils/utils";
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from "../types";
+import { newEntrySchema } from "../utils/utils";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -19,18 +20,40 @@ router.get("/:id", (req, res) => {
   }
 });
 
-router.post("/", (req: Request, res: Response) => {
-  try {
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
-    res.json(addedEntry);
-  } catch (error: unknown) {
-    let errorMessage = "Something went wrong.";
-    if (error instanceof Error) {
-      errorMessage += " Error " + error.message;
-    }
-    res.status(400).send(errorMessage);
+const errorMiddleware = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
   }
-});
+};
+
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    newEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+router.post(
+  "/",
+  newDiaryParser,
+  (
+    req: Request<unknown, unknown, NewDiaryEntry>,
+    res: Response<DiaryEntry>
+  ) => {
+    const addedEntry = diaryService.addDiary(req.body);
+    res.json(addedEntry);
+  }
+);
+
+router.use(errorMiddleware);
 
 export default router;
--- ilari/src/types.ts
+++ ilari/src/types.ts
@@ -1,16 +1,19 @@
+import { z } from "zod";
+import { newEntrySchema } from "./utils/utils";
+
 export enum Weather {
-  Sunny = 'sunny',
-  Rainy = 'rainy',
-  Cloudy = 'cloudy',
-  Stormy = 'stormy',
-  Windy = 'windy',
+  Sunny = "sunny",
+  Rainy = "rainy",
+  Cloudy = "cloudy",
+  Stormy = "stormy",
+  Windy = "windy",
 }
 
 export enum Visibility {
-  Great = 'great',
-  Good = 'good',
-  Ok = 'ok',
-  Poor = 'poor',
+  Great = "great",
+  Good = "good",
+  Ok = "ok",
+  Poor = "poor",
 }
 
 export interface DiaryEntry {
@@ -21,6 +24,6 @@ export interface DiaryEntry {
   comment?: string;
 }
 
-export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
+export type NewDiaryEntry = z.infer<typeof newEntrySchema>;
 
-export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, "comment">;
